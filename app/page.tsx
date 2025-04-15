73f4609dd95f0ff88744a4d559992a5329357aa5
ThiagoC0STA <thiago@lacostacorretora.com.br>
2025-04-15 13:32:13 -0300

    bar row

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { ChartBarRow } from "@/components/chart-bar-row"
 import { ChartPieRow } from "@/components/chart-pie-row"
 import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
@@ -29,6 +30,7 @@ export default function Page() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
               <ChartPieRow />
+              <ChartBarRow />
               <div className="px-4 lg:px-6">
                 <ChartAreaInteractive />
               </div>
