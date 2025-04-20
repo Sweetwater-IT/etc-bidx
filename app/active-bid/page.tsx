16de08d111710cd29ceb81b5165691b945cd66b9
UlugbekAtakhanov <75473856+UlugbekAtakhanov@users.noreply.github.com>
2025-04-20 22:43:06 +0500

    Apr20, 22:40

--- app/active-bid/page.tsx
+++ app/active-bid/page.tsx
@@ -46,6 +46,7 @@ export default function ActiveBidPage() {
                                     <h1 className="text-3xl font-bold">Create New Bid</h1>
                                 </div>
 
+                                {/* main steps */}
                                 <StepsMain />
                             </div>
                         </div>
