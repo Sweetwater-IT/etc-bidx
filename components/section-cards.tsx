3c97f2ef7cd44f7f3c1f1bf9971e4add15f7fa18
Louis Fairbanks <base216ball@protonmail.com>
2025-05-07 15:25:32 -0300

    fix: revert dashboard and other cards

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -1,15 +1,21 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import type { CardData } from "@/data/jobs-cards";
 
 export function SectionCards({ data }: { data: CardData[] }) {
     return (
+
         <div className="px-4 lg:px-6">
-            <div className="grid grid-cols-6 gap-4">
+            <div className="flex flex-wrap w-full gap-4">
                 {data.map((card, index) => (
-                    <Card key={index} className="flex flex-col !py-4">
+                    <Card
+                        key={index}
+                        className="@container/card grow basis-0"
+                    >
                         <CardHeader>
                             <CardDescription>{card.title}</CardDescription>
-                            <CardTitle className="font-semibold">{card.value}</CardTitle>
+                            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                                {card.value}
+                            </CardTitle>
                         </CardHeader>
                     </Card>
                 ))}
