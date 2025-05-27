1fce2392597e0e928959e33f3730ee76f2f738ec
ThiagoC0STA <thiago@lacostacorretora.com.br>
2025-05-27 11:49:28 -0300

    adjust

--- components/pages/active-bid/steps/add-phase-button.tsx
+++ components/pages/active-bid/steps/add-phase-button.tsx
@@ -1,32 +1,31 @@
-import { Button } from '@/components/ui/button'
-import { useEstimate } from '@/contexts/EstimateContext'
-import React, { Dispatch, SetStateAction } from 'react'
+import { Button } from "@/components/ui/button";
+import { useEstimate } from "@/contexts/EstimateContext";
+import React, { Dispatch, SetStateAction } from "react";
 
 interface Props {
-    setCurrentPhase : Dispatch<SetStateAction<number>>
-    setCurrentStep : Dispatch<SetStateAction<number>>
+  setCurrentPhase: Dispatch<SetStateAction<number>>;
+  setCurrentStep: Dispatch<SetStateAction<number>>;
 }
 
-const AddPhaseButton = ({ setCurrentPhase, setCurrentStep} : Props) => {
+const AddPhaseButton = ({ setCurrentPhase, setCurrentStep }: Props) => {
+  const { dispatch, mptRental } = useEstimate();
 
-    const { dispatch, mptRental } = useEstimate()
+  const handlePhaseClick = () => {
+    dispatch({ type: "ADD_MPT_PHASE" });
+    setCurrentPhase(mptRental.phases.length);
+    setCurrentStep(2);
+  };
 
-    const handlePhaseClick = () => {
-      dispatch({ type : 'ADD_MPT_PHASE'  })
-      setCurrentPhase(mptRental.phases.length)
-      setCurrentStep(2)
-    }
-  
   return (
-    <Button 
-      className="z-0 relative" 
-      style={{ marginLeft: 'auto', display: 'block', width: '100%'}} 
+    <Button
+      className="z-0 relative"
+      style={{ marginLeft: "auto", display: "block", width: "100%" }}
       onClick={handlePhaseClick}
       title="Add a New Phase to the Bid"
     >
       Add Phase
     </Button>
-  )
-}
+  );
+};
 
-export default AddPhaseButton
+export default AddPhaseButton;
