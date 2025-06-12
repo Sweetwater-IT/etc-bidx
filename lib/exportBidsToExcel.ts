19408c18a5c9d76730b0a55d09c76a4b9768c73b
Louis Fairbanks <base216ball@protonmail.com>
2025-06-12 00:43:58 -0300

    fix: takeoff extra conditional operators

--- lib/exportBidsToExcel.ts
+++ lib/exportBidsToExcel.ts
@@ -63,7 +63,7 @@ const formatDateForExcel = (date: Date | null | undefined): string => {
 const getCustomItems = (data: EstimateData[]) => {
     const customItems = new Set<string>();
     data.forEach(row => {
-        row?.mpt_rental?.phases ? row?.mpt_rental?.phases.forEach(phase => {
+        row.mpt_rental?.phases ? row.mpt_rental?.phases.forEach(phase => {
             if (phase.customLightAndDrumItems) {
                 phase.customLightAndDrumItems.forEach((item : CustomLightAndDrumItem) => {
                     if (item.id) customItems.add(item.id);
