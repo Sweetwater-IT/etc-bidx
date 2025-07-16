4ce90f139c533a7b776b8b5c295fa42737261211
Kenny Mack <knnymck@icloud.com>
2025-07-16 12:49:52 -0500

    Update EstimateReducer.ts

--- contexts/EstimateReducer.ts
+++ contexts/EstimateReducer.ts
@@ -378,7 +378,7 @@ export const estimateReducer = (
 					...state.mptRental,
 					phases: state.mptRental.phases.map((phase: Phase) => ({
 						...phase,
-						signs: phase.signs.filter((sign) => sign.id !== signIdToDelete),
+						signs: phase.signs.filter((sign) => sign.id !== signId),
 					})),
 				},
 			};
