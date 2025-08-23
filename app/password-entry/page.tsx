767c12a6654d8c03d70ec0dff1090990fc6b4ac1
Rocady21 <rodrigo.olivera@estudiantes.utec.edu.uy>
2025-08-23 10:08:13 -0300

    fix redirectTo

--- app/password-entry/page.tsx
+++ app/password-entry/page.tsx
@@ -47,7 +47,7 @@ export default function GoogleAuthPage() {
               const { error } = await supabase.auth.signInWithOAuth({
                 provider: "google",
                 options: {
-                  redirectTo: `http://localhost:3000/auth/callback`,
+                  redirectTo: `${window.location.origin}/auth/callback`,
                 },
               });
 
