da5d84ec979f0377039b85f9a763b3a535b5f3b0
reddev211 <215572727+reddev211@users.noreply.github.com>
2025-09-16 05:25:18 -0500

    Update mptRentalHelperFunctions.ts

--- lib/mptRentalHelperFunctions.ts
+++ lib/mptRentalHelperFunctions.ts
@@ -846,7 +846,7 @@ export function getNonRatedHoursPerPhase(adminData: AdminData, phase: Phase): nu
   const totalTravelTimeMins = (adminData.owTravelTimeHours !== undefined && adminData.owTravelTimeMinutes !== undefined)
     ? safeNumber(adminData.owTravelTimeHours) * 60 + safeNumber(adminData.owTravelTimeMinutes)
     : safeNumber(adminData.owTravelTimeMins);
-  const nonRatedHours = ((totalTravelTimeMins / 60) * totalTrips * 2) * phase.personnel;
+  const nonRatedHours = ((totalTravelTimeMins / 60) * totalTrips) * phase.personnel;
 
   return nonRatedHours;
 }
@@ -863,7 +863,7 @@ export function getTotalTripsPerPhase(phase: Phase): number {
   const postQuantity = phase.standardEquipment.post?.quantity || 0;
 
   const relevantEquipmentTotals = fourFootQuantity + hStandQuantity + postQuantity;
-  return safeNumber(phase.maintenanceTrips) + (Math.ceil(relevantEquipmentTotals / 30) * 2);
+  return safeNumber(phase.maintenanceTrips) + Math.ceil(relevantEquipmentTotals / 30);
 }
 
 export function calculateFlaggingCostSummary(adminData: AdminData, flagging: Flagging, isServiceWork: boolean): FlaggingSummary {
