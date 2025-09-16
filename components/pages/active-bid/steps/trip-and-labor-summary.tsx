93b6141a269e48bcbbaaedb43b1d37176c05e2ad
reddev211 <215572727+reddev211@users.noreply.github.com>
2025-09-16 06:37:26 -0500

    Update trip-and-labor-summary.tsx

--- components/pages/active-bid/steps/trip-and-labor-summary.tsx
+++ components/pages/active-bid/steps/trip-and-labor-summary.tsx
@@ -105,7 +105,7 @@ export const TripAndLaborSummary = ({
       <div className='flex flex-col'>
         <label className='text-sm font-semibold'>Total Mobilizations</label>
         <div className='pr-3 py-1 select-text cursor-default text-muted-foreground'>
-          {safeNumber(baseTrips + safeNumber(phase.maintenanceTrips))}
+          {safeNumber(totalTrips)}
         </div>
       </div>
 
