d320140f82e303f1462337d16372ed5e4cea0ee3
Kenny Mack <knnymck@icloud.com>
2025-12-12 11:45:21 -0600

    Update get-help-modal.tsx

--- components/get-help-modal.tsx
+++ components/get-help-modal.tsx
@@ -2,7 +2,7 @@
 
 import type React from "react"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
 import { Input } from "@/components/ui/input"
