a7fc8fb7a147940593043dbe69dab0599414f191
Kenny Mack <knnymck@icloud.com>
2026-01-28 09:06:40 -0600

    Update add-item-modal.tsx

--- components/add-item-modal.tsx
+++ components/add-item-modal.tsx
@@ -8,8 +8,8 @@ import { Separator } from "@/components/ui/separator"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { toast } from 'sonner'
-import { createDailyTrackerEntry } from '@/hooks/use-daily-tracker-swr'
-import { DailyTrackerEntry } from '@/types/DailyTrackerEntry'
+//import { createDailyTrackerEntry } from '@/hooks/use-daily-tracker-swr'
+//import { DailyTrackerEntry } from '@/types/DailyTrackerEntry'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 interface AddItemModalProps {
